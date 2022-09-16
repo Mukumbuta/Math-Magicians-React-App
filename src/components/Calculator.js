1328d4ad925c31e989a49972b1cb3e9ef50a9150
Mukumbuta <mukumbutasimasiku@gmail.com>
2022-09-16 14:52:01 -0700

    add eventListener on Button components in order to lift up state

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,44 +1,65 @@
 import React, { Component } from 'react';
+import calculate from '../logic/calculate';
 import Button from './Button';
 import Display from './Display';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Calculator extends Component {
+  constructor() {
+    super();
+    this.clickHandler = this.clickHandler.bind(this);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  clickHandler(e) {
+    // eslint-disable-next-line react/no-access-state-in-setstate
+    this.setState(calculate(this.state, e.target.textContent));
+    // console.log(e.target.textContent)
+    // console.log(this.state)
+  }
+
   render() {
+    const { total } = this.state;
+    const clickFunc = this.clickHandler;
+
     return (
       <div className="calc-frame">
         <div className="main-calc">
-          <Display />
+          <Display value={total || '0'} />
           <div className="btn-row">
-            <Button label="AC" />
-            <Button label="+/-" />
-            <Button label="%" />
-            <Button label="÷" />
+            <Button eventsHandler={clickFunc} label="AC" />
+            <Button eventsHandler={clickFunc} label="+/-" />
+            <Button eventsHandler={clickFunc} label="%" />
+            <Button eventsHandler={clickFunc} label="÷" />
           </div>
           <div className="btn-row">
-            <Button label="7" />
-            <Button label="8" />
-            <Button label="9" />
-            <Button label="×" />
+            <Button eventsHandler={clickFunc} label="7" />
+            <Button eventsHandler={clickFunc} label="8" />
+            <Button eventsHandler={clickFunc} label="9" />
+            <Button eventsHandler={clickFunc} label="x" />
           </div>
           <div className="btn-row">
-            <Button label="4" />
-            <Button label="5" />
-            <Button label="6" />
-            <Button label="+" />
+            <Button eventsHandler={clickFunc} label="4" />
+            <Button eventsHandler={clickFunc} label="5" />
+            <Button eventsHandler={clickFunc} label="6" />
+            <Button eventsHandler={clickFunc} label="+" />
           </div>
           <div className="btn-row">
-            <Button label="1" />
-            <Button label="2" />
-            <Button label="3" />
-            <Button label="-" />
+            <Button eventsHandler={clickFunc} label="1" />
+            <Button eventsHandler={clickFunc} label="2" />
+            <Button eventsHandler={clickFunc} label="3" />
+            <Button eventsHandler={clickFunc} label="-" />
           </div>
           <div className="last-btn-row btn-row">
             <div className="zero-btn">
-              <Button label="0" />
+              <Button eventsHandler={clickFunc} label="0" />
             </div>
-            <Button label="." />
-            <Button label="=" />
+            <Button eventsHandler={clickFunc} label="." />
+            <Button eventsHandler={clickFunc} label="=" />
           </div>
         </div>
       </div>
