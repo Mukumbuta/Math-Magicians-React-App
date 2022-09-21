d340c0bcb158961df9bdddea1640a3d554f1b2f2
Mukumbuta <mukumbutasimasiku@gmail.com>
2022-09-21 16:17:50 -0700

    added all page components to the BrowserRouter Router component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import Calc from './components/Calculator';
+import { Route, Routes } from 'react-router-dom';
+import Calc from './Components/Calculator';
+import Home from './Pages/Home';
+import Quote from './Pages/Quotes';
+import Nav from './Components/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <Calc />
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Nav />
+    <Routes>
+      <Route exact path="/home" element={<Home />} />
+      <Route exact path="/calculator" element={<Calc />} />
+      <Route exact path="/quotes" element={<Quote />} />
+    </Routes>
+  </div>
+);
 
 export default App;
